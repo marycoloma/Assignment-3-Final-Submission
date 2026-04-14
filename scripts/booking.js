bbc4cb08a2ce5babaa435456f24bf5427fed14e0
mscoloma <MaryAnn.Coloma@edu.sait.ca>
2026-04-14 11:58:36 -0600

    clear days logic

--- scripts/booking.js
+++ scripts/booking.js
@@ -20,7 +20,13 @@ daySelectors.forEach(function (day) {
 });
 
 /********* clear days *********/
-// when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
+clearSelectButton.addEventListener("click", function () {
+  daySelectors.forEach(function (day) {
+    day.classList.remove("clicked");
+  });
+  dayCounter = 0;
+  calculateCost();
+});
 
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
